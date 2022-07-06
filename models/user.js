0111648c7213dfc98595bca5501b143d1849517c
Dylan Okum <dylanokum@icloud.com>
2022-07-06 01:27:47 -0400

    added bio questions to user model

--- models/user.js
+++ models/user.js
@@ -1,2 +1,57 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
+
+module.exports = function(sequelize, Sequelize) {
+ 
+    var User = sequelize.define('user', {
+ 
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER
+        },
+ 
+        firstName: {
+            type: Sequelize.STRING,
+            notEmpty: true
+        },
+ 
+        lastName: {
+            type: Sequelize.STRING,
+            notEmpty: true
+        },
+ 
+        username: {
+            type: Sequelize.TEXT
+        },
+ 
+        dogBreed: {
+            type: Sequelize.TEXT
+        },
+
+        dogWeight: {
+            type: Sequelize.TEXT
+        },
+ 
+        email: {
+            type: Sequelize.STRING,
+            validate: {
+                isEmail: true
+            }
+        },
+ 
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+ 
+        last_login: {
+            type: Sequelize.DATE
+        },
+
+    });
+ 
+    return User;
+ 
+}
