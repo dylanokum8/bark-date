8c6017b1e13e1fd7ba26269d9bb27e588d916f75
Dylan Okum <dylanokum@icloud.com>
2022-07-06 01:21:07 -0400

    wrote more starter code and added more of the folders needed for the file structure

--- server.js
+++ server.js
@@ -1,4 +1,30 @@
-const express = require('express');
-const routes = require('./routes');
 
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const app = express();
 const PORT = process.env.PORT || 3001;
+const sequelize = require('./config/connection');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// sync sequelize models to the database, then turn on the server
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
