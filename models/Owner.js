059d100b1597b48ce3e573dd094014dc6af3ced3
Qu33nArlene <ahaskins93@yahoo.com>
2022-07-06 19:48:26 -0400

    added seed files. added model files. edited user-owner file

---
+++ models/Owner.js
@@ -0,0 +1,71 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+
+// create our Owner model
+class Owner extends Model {
+  // set up method to run on instance data (per owner) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+// create fields/columns for Owner model
+Owner.init(
+  {
+    username: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4],
+      },
+    },
+  },
+  {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newOwnerData) {
+        newOwnerData.password = await bcrypt.hash(newOwnerData.password, 10);
+        return newOwnerData;
+      },
+
+      async beforeUpdate(updatedOwnerData) {
+        updatedOwnerData.password = await bcrypt.hash(
+          updatedOwnerData.password,
+          10
+        );
+        return updatedOwnerData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Owner",
+  }
+);
+
+module.exports = Owner;
---
+++ models/index.js
@@ -0,0 +1,24 @@
+//=================================================
+//EXAMPLE CODE
+// // import all models
+// const Post = require("./Post");
+// const User = require("./User");
+// const Comment = require("./Comment");
+
+// // create associations
+// Post.hasMany(Comment, {
+//   foreignKey: "post_id",
+// });
+
+// Post.belongsTo(User, {
+//   foreignKey: "user_id",
+//   onDelete: "SET NULL",
+// });
+
+// Comment.belongsTo(User, {
+//   foreignKey: "user_id",
+//   onDelete: "SET NULL",
+// });
+
+// module.exports = { User, Post, Comment };
+//=================================================
