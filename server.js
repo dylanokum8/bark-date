5ed75cc78f2470a4ed0e5897ccc90c7d6e7e2214
Dylan Okum <dylanokum@icloud.com>
2022-07-06 14:17:05 -0400

    added login handlebars

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const passport = require('passport');
+const PORT = process.env.PORT || 3006;
 const sequelize = require('./config/connection');
 
 
