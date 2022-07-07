39f72c77c7771b27685dc9e7629c8c5956692dde
Qu33nArlene <ahaskins93@yahoo.com>
2022-07-06 20:48:28 -0400

    created seeds for Dog and Owner files

---
+++ seeds/Owner-seeds.js
@@ -0,0 +1,51 @@
+const sequelize = require("../config/connection");
+const { Owner, Dog } = require("../models");
+
+const ownerData = [
+  {
+    username: "A-Hask",
+    firstname: "Arlene",
+    lastname: "Hask",
+    email: "arlene@email.com",
+  },
+  {
+    username: "reesewitherspoon",
+    firstname: "Reese",
+    lastname: "Witherspoon",
+    email: "reese@email.com",
+  },
+  {
+    username: "parishilton",
+    firstname: "Paris",
+    lastname: "Hilton",
+    email: "paris@email.com",
+  },
+  {
+    username: "demilovato",
+    firstname: "Demi",
+    lastname: "Lovato",
+    email: "demi@email.com",
+  },
+  {
+    username: "barackobama",
+    firstname: "Barack",
+    lastname: "Obama",
+    email: "barack@email.com",
+  },
+  {
+    username: "chrisevans",
+    firstname: "Chris",
+    lastname: "Evans",
+    email: "chris@email.com",
+  },
+  {
+    username: "tomholland",
+    firstname: "Tom",
+    lastname: "Holland",
+    email: "tom@email.com",
+  },
+];
+
+const seedOwners = () => Owner.bulkCreate(ownerData, { individualHooks: true });
+
+module.exports = seedOwners;
