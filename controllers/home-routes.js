51a4229cde3039f98a295b95f98b5f7369fc77e2
dan <danielornelas01@yahoo.com>
2022-07-09 20:08:02 -0700

    signup page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,21 @@
 const router = require("express").Router();
 
+// Import the custom middleware
+const withAuth = require('../utils/auth');
+
+//homepage
 router.get("/", async (req, res) => {
   res.render("home");
 });
 
+//login page
 router.get("/login", (req, res) => {
   res.render("login", { main: ["daniel", "erick", "john"] });
 });
+
+//sign up page
+router.get("/signup", (req, res) => {
+    res.render("signup")
+})
 
 module.exports = router;
