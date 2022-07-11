a8d945c107895b7610425ad7adabf975d83d73e0
dan <danielornelas01@yahoo.com>
2022-07-11 12:31:54 -0700

    .

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,75 +2,75 @@ const router = require('express').Router();
 const { Owner } = require('../../models');
 
 // CREATE new user
-router.post('/', async (req, res) => {
-  try {
-    const dbOwnerData = await Owner.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
+// router.post('/', async (req, res) => {
+//   try {
+//     const dbOwnerData = await Owner.create({
+//       username: req.body.username,
+//       email: req.body.email,
+//       password: req.body.password,
+//     });
 
-    // Set up sessions with a 'loggedIn' variable set to `true`
-    req.session.save(() => {
-      req.session.loggedIn = true;
+//     // Set up sessions with a 'loggedIn' variable set to `true`
+//     req.session.save(() => {
+//       req.session.loggedIn = true;
 
-      res.status(200).json(dbOwnerData);
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
+//       res.status(200).json(dbOwnerData);
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
 
-// Login
-router.post('/login', async (req, res) => {
-  try {
-    const dbOwnerData = await Owner.findOne({
-      where: {
-        email: req.body.email,
-      },
-    });
+// // Login
+// router.post('/login', async (req, res) => {
+//   try {
+//     const dbOwnerData = await Owner.findOne({
+//       where: {
+//         email: req.body.email,
+//       },
+//     });
 
-    if (!dbOwnerData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password. Please try again!' });
-      return;
-    }
+//     if (!dbOwnerData) {
+//       res
+//         .status(400)
+//         .json({ message: 'Incorrect email or password. Please try again!' });
+//       return;
+//     }
 
-    const validPassword = await dbOwnerData.checkPassword(req.body.password);
+//     const validPassword = await dbOwnerData.checkPassword(req.body.password);
 
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password. Please try again!' });
-      return;
-    }
+//     if (!validPassword) {
+//       res
+//         .status(400)
+//         .json({ message: 'Incorrect email or password. Please try again!' });
+//       return;
+//     }
 
-    // Once the user successfully logs in, set up the sessions variable 'loggedIn'
-    req.session.save(() => {
-      req.session.loggedIn = true;
+//     // Once the user successfully logs in, set up the sessions variable 'loggedIn'
+//     req.session.save(() => {
+//       req.session.loggedIn = true;
 
-      res
-        .status(200)
-        .json({ user: dbOwnerData, message: 'You are now logged in!' });
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
+//       res
+//         .status(200)
+//         .json({ user: dbOwnerData, message: 'You are now logged in!' });
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
 
-// Logout
-router.post('/logout', (req, res) => {
-  // When the user logs out, destroy the session
-  if (req.session.loggedIn) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
-});
+// // Logout
+// router.post('/logout', (req, res) => {
+//   // When the user logs out, destroy the session
+//   if (req.session.loggedIn) {
+//     req.session.destroy(() => {
+//       res.status(204).end();
+//     });
+//   } else {
+//     res.status(404).end();
+//   }
+// });
 
 module.exports = router;
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -5,7 +5,7 @@ const loginFormHandler = async (event) => {
     const password = document.querySelector('#password-login').value.trim();
   
     if (email && password) {
-      const response = await fetch('/login', {
+      const response = await fetch('/', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
@@ -23,19 +23,22 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
   
     const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
     const firstname = document.querySelector('#firstname-signup').value.trim();
     const lastname = document.querySelector('#lastname-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
     const phonenumber = document.querySelector('#phone-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
     const dogname = document.querySelector('#dogname-signup').value.trim();
     const dogbreed = document.querySelector('#dogbreed-signup').value.trim();
     const dogweight = document.querySelector('#dogweight-signup').value.trim();
+    const male = document.querySelector('#male-signup').value.trim();
+    const female = document.querySelector('#female-signup').value.trim();
   
-    if (username && firstname && lastname && email && phonenumber && password && dogname && dogbreed && dogweight) {
-      const response = await fetch('/signup', {
+    if (username && firstname && lastname && email && phonenumber && password && dogname && dogbreed && dogweight && male && female) {
+      const response = await fetch('/', {
         method: 'POST',
-        body: JSON.stringify({ username, firstname, lastname, email, phonenumber, password, dogname, dogbreed, dogweight }),
+        body: JSON.stringify({ username, firstname, lastname, email, phonenumber, password, dogname, dogbreed, dogweight, male, female }),
         headers: { 'Content-Type': 'application/json' },
       });
   
