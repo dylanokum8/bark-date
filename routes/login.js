78d7a0125d3959611fa34235e058828ca701a05a
dan <danielornelas01@yahoo.com>
2022-07-12 10:41:37 -0700

    env

--- routes/login.js
+++ routes/login.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const router = express.Router();
-const passport = require('passport');
+// const express = require('express');
+// const router = express.Router();
+// const passport = require('passport');
 
-/* GET Login Index page. */
-router.get('/', function(req, res) {
-    res.render('index');
-});
+// /* GET Login Index page. */
+// router.get('/', function(req, res) {
+//     res.render('index');
+// });
 
-/* POST Validate the user login */
-router.post('/validate', passport.authenticate('local', { failureRedirect: '/' }),
-    function(req, res) {
-        res.redirect('/main');
-    });
+// /* POST Validate the user login */
+// router.post('/validate', passport.authenticate('local', { failureRedirect: '/' }),
+//     function(req, res) {
+//         res.redirect('/main');
+//     });
 
-module.exports = router;
+// module.exports = router;
