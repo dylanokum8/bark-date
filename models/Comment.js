d5a3342a06c3808adfa4531c3ca4fa523fc998e0
dan <danielornelas01@yahoo.com>
2022-07-11 22:33:43 -0700

    post and post routes

--- models/Comment.js
+++ models/Comment.js
@@ -5,13 +5,33 @@ class Comment extends Model {}
 
 Comment.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
     comment_text: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [1],
-      },
+        len: [1]
+      }
     },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
--- models/Post.js
+++ models/Post.js
@@ -4,11 +4,36 @@ const sequelize = require("../config/connection");
 class Post extends Model {}
 Post.init(
   {
-    title: DataTypes.STRING,
-    body: DataTypes.STRING,
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    post_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isURL: true
+      }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post'
   }
 );
 
