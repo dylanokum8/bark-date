f8e99777220fcbc47efee48526e0584b0b0ac13c
Qu33nArlene <ahaskins93@yahoo.com>
2022-07-13 15:07:11 -0400

    adjusted get router for owner in profile-routes

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -6,21 +6,14 @@ const withAuth = require("../utils/auth");
 //enter owner and dog info in profile page
 router.get("/:id", withAuth, (req, res) => {
   Owner.findByPk(req.params.id, {
-<<<<<<< HEAD
-    // include: [Dog],
-=======
     where: {
       id: req.params.id,
     },
     include: [Dog],
->>>>>>> feature/profileViews
   })
     .then((dbOwnerData) => {
       const owner = dbOwnerData.get({ plain: true });
-      console.log(owner);
-      res.render("profile", {
-        owner,
-      });
+      res.render("profile", owner);
     })
     .catch((err) => {
       console.log(err);
