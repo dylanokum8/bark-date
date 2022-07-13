4c642d19a1592d772a82898424c8ac9987304509
Qu33nArlene <ahaskins93@yahoo.com>
2022-07-13 16:47:56 -0400

    fixed more merge conflicts

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -6,20 +6,11 @@ const withAuth = require("../utils/auth");
 //enter owner and dog info in profile page
 router.get("/:id", withAuth, (req, res) => {
   Owner.findByPk(req.params.id, {
-<<<<<<< HEAD
-    where: {
-      id: req.params.id,
-    },
-    include: [Dog],
-=======
-
-   include: [
-    {
-      model: Dog
-    }
-   ]
-
->>>>>>> 5cecdae1315394cd85373ea9d14b7b22ba501dd1
+    include: [
+      {
+        model: Dog,
+      },
+    ],
   })
     .then((dbOwnerData) => {
       const owner = dbOwnerData.get({ plain: true });
